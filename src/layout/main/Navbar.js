5a5874544425db4739181a587e3d1be90323af56
md Hamimul Haue <mdhamimulhaque@gmail.com>
2023-12-02 16:39:53 +0600

    login user done

--- src/layout/main/Navbar.js
+++ src/layout/main/Navbar.js
@@ -1,10 +1,11 @@
-import { signOut } from "firebase/auth";
 import React from "react";
+import { useSelector } from "react-redux";
 
 import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
   const { pathname } = useLocation();
+  const { email } = useSelector((state) => state.auth);
 
   return (
     <nav
@@ -12,23 +13,29 @@ const Navbar = () => {
         pathname === "/" ? null : "bg-white"
       }`}
     >
-      <ul className='max-w-7xl mx-auto flex gap-3 h-full items-center'>
-        <li className='flex-auto font-semibold text-2xl'>
-          <Link to='/'>JobBox</Link>
+      <ul className="max-w-7xl mx-auto flex gap-3 h-full items-center">
+        <li className="flex-auto font-semibold text-2xl">
+          <Link to="/">JobBox</Link>
         </li>
         <li>
-          <Link className='hover:text-primary' to='/jobs'>
+          <Link className="hover:text-primary" to="/jobs">
             Jobs
           </Link>
         </li>
 
         <li>
-          <Link
-            className='border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all '
-            to='/login'
-          >
-            Login
-          </Link>
+          {!email ? (
+            <Link
+              className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
+              to="/login"
+            >
+              Login
+            </Link>
+          ) : (
+            <button className="border border-red-500 px-2 py-1 rounded-full hover:border-red-600 hover:text-white hover:bg-red-400 hover:px-4 transition-all ">
+              LogOut
+            </button>
+          )}
         </li>
       </ul>
     </nav>
